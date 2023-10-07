56a01f83d8ad4b8b7f28f4f10db113d524ed71f6
Ben <benblackk95@gmail.com>
2023-10-07 18:06:31 -0400

    init adding s axis

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,21 @@
 #include <AccelStepper.h> //accelstepper library
-AccelStepper stepper(1, 2, 38); 
-//AccelStepper stepper2(1, 10, 11); // direction Digital 11 (CCW), pulses Digital 10 (CLK)
-
+AccelStepper Rstepper(1, 2, 38); //pulses Digital 2 (CLK), direction Digital 38 
+AccelStepper S1stepper(1, 10, 11); //pulses Digital 10 (CLK), direction Digital 11 
+AccelStepper S2stepper(1, 10, 11); //pulses Digital 10 (CLK), direction Digital 11 
 
 //Pins
-const byte Analog_X_pin = A1; //x-axis readings
-// const byte Analog_Y_pin = A1; //y-axis readings
-// const byte Analog_R_pin = A2; //r-axis readings
+const byte Analog_R_pin = A1; //r - 'rotation'-axis readings
+const byte Analog_S_pin = A2; //s - 'shoulder'-axis readings
+// const byte Analog_E_pin = A2; //e - 'elbow'-axis readings
 // const byte LED_pin = 3; //PWM output for LED
 
 //Variables
-int Analog_X = 0; //x-axis value
-// int Analog_Y = 0; //y-axis value
+int Analog_R = 0; //x-axis value
+int Analog_S = 0; //y-axis value
 // int Analog_R = 0; //r-axis value
 
-int Analog_X_AVG = 0; //x-axis value average
-// int Analog_Y_AVG = 0; //y-axis value average
+int Analog_R_AVG = 0; //x-axis value average
+int Analog_S_AVG = 0; //y-axis value average
 // int Analog_R_AVG = 0; //r-axis value average
 
 //int Analog_R_Value = 0; //this is used for the PWM value
@@ -24,15 +24,12 @@ unsigned long prevMillis = 0;  // ms
 
 void setup()
 {
-
   // Schedule sampling interval
-
-
   //SERIAL
   Serial.begin(9600);
   //----------------------------------------------------------------------------    
   //PINS
-  pinMode(Analog_X_pin, INPUT);
+  pinMode(Analog_R_pin, INPUT);
   // pinMode(Analog_Y_pin, INPUT);  
   // pinMode(Analog_R_pin, INPUT); 
   // pinMode(LED_pin, OUTPUT);
@@ -41,9 +38,9 @@ void setup()
   //----------------------------------------------------------------------------  
   //Stepper parameters
   //setting up some default values for maximum speed and maximum acceleration
-  stepper.setMaxSpeed(120); //SPEED = Steps / second  
-  stepper.setAcceleration(100); //ACCELERATION = Steps /(second)^2    
-  stepper.setSpeed(100);
+  Rstepper.setMaxSpeed(120); //SPEED = Steps / second  
+  Rstepper.setAcceleration(100); //ACCELERATION = Steps /(second)^2    
+  Rstepper.setSpeed(100);
   delay(500);
   //----------------------------------------------------------------------------
   //stepper2.setMaxSpeed(5000); //SPEED = Steps / second  
@@ -59,7 +56,9 @@ void loop()
   if (millis() - prevMillis >= interval) {
    ReadAnalog();
   }   
-  stepper.runSpeed(); //step the motor (this will step the motor by 1 step at each loop indefinitely)
+  Rstepper.runSpeed(); //step the motor (this will step the motor by 1 step at each loop indefinitely)
+  S1stepper.runSpeed();
+  S2stepper.runSpeed();
 
   // Serial.println("clockwise");
   // stepper.setSpeed(100);
@@ -78,67 +77,65 @@ void loop()
 void ReadAnalog()
 {
   //Reading the 3 potentiometers in the joystick: x, y and r.
-  Analog_X = analogRead(Analog_X_pin);  
-  // Analog_Y = analogRead(Analog_Y_pin);    
+  Analog_R = analogRead(Analog_R_pin);  
+  Analog_S = analogRead(Analog_S_pin);    
   // Analog_R = analogRead(Analog_R_pin); 
   //Serial.println(Analog_X);  
 
   //if the value is 25 "value away" from the average (midpoint), we allow the update of the speed
   //This is a sort of a filter for the inaccuracy of the reading
-  if(abs(Analog_X-Analog_X_AVG)>25) 
+  if(abs(Analog_R-Analog_R_AVG)>25) 
   {
-    int setSpeed = 7*(Analog_X-Analog_X_AVG);
-  stepper.setSpeed(setSpeed);  
-  Serial.println(setSpeed);     
+    int setSpeed = 7*(Analog_R-Analog_R_AVG);
+    Rstepper.setSpeed(setSpeed);  
+    //Serial.println(setSpeed);     
   }
   else
   {
-    stepper.setSpeed(0);
+    Rstepper.setSpeed(0);
   }
-  //----------------------------------------------------------------------------  
-  // if(abs(Analog_Y-Analog_Y_AVG)>25) 
-  // {
-  // stepper2.setSpeed(5*(Analog_Y-Analog_Y_AVG));  
-  // }
-  // else
-  // {
-  //   stepper2.setSpeed(0);
-  // }
-  // //----------------------------------------------------------------------------  
-  // if(abs(Analog_R-Analog_R_AVG)>25) 
-  // {
-  //   Analog_R_Value = map(Analog_R, 0, 1023, 0, 255); //10 bit ADC (0-1023) and 8 bit PWM (0-255)
-  //   analogWrite(LED_pin, Analog_R_Value); //modify the PWM value     
-  // }
+  
+  //Here we will flip the direction of 1 of the motors.  
+  if(abs(Analog_S-Analog_S_AVG)>25) 
+  {
+    S1stepper.setSpeed(5*(Analog_S-Analog_S_AVG));  
+    S2stepper.setSpeed(-5*(Analog_S-Analog_S_AVG));  
+  }
+  else
+  {
+    S1stepper.setSpeed(0);
+    S2stepper.setSpeed(0);
+  }
 }
 
 void InitialValues()
 {
   //Set the values to zero before averaging
-  float tempX = 0;
-  // float tempY = 0;
+  float tempR = 0;
+  float tempS = 0;
   // float tempR = 0;
   //----------------------------------------------------------------------------  
   //read the analog 50x, then calculate an average. 
   //they will be the reference values
   for(int i = 0; i<50; i++)
   {
-     tempX += analogRead(Analog_X_pin);  
-     delay(10); //allowing a little time between two readings
-    //  tempY += analogRead(Analog_Y_pin);        
-    //  delay(10);
+    tempR += analogRead(Analog_R_pin);  
+    delay(10); //allowing a little time between two readings
+    tempS += analogRead(Analog_S_AVG);        
+    delay(10);
     //  tempR += analogRead(Analog_R_pin);
     //  delay(10);
   }
   //----------------------------------------------------------------------------  
-  Analog_X_AVG = tempX/50; 
+  Analog_R_AVG = tempR/50; 
+  Analog_S_AVG = tempS/50;
   // Analog_Y_AVG = tempY/50; 
   // Analog_R_AVG = tempR/50; 
   //----------------------------------------------------------------------------  
-  Serial.print("AVG_X: ");
-  Serial.println(Analog_X_AVG);
-  // Serial.print("AVG_Y: ");
-  // Serial.println(Analog_Y_AVG);
+  Serial.print("AVG_R (Rotation): ");
+  Serial.println(Analog_R_AVG);
+  Serial.print("AVG_S (Shoulder): ");
+  Serial.println(Analog_S_AVG);
   // Serial.print("AVG_R: ");
   // Serial.println(Analog_R_AVG);
   Serial.println("Calibration finished");  
